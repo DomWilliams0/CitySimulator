f4647be548c93eb34c5f206414320fda023c45a1
Dom Williams <dwi3774@googlemail.com>
2015-06-13 16:40:08 +0100

    Abstracted Game, to allow for spikes

--- CitySimulator/src/game.cpp
+++ CitySimulator/src/game.cpp
@@ -6,15 +6,13 @@
 #include "state.hpp"
 #include "utils.hpp"
 
-Game::Game(const sf::Vector2i &windowSize, const sf::Uint32 &style) :
-	window(sf::RenderWindow(sf::VideoMode(windowSize.x, windowSize.y), "Dank C++ Memes", style))
+BaseGame::BaseGame(const sf::Vector2i &windowSize, const sf::Uint32 &style, const std::string &title) :
+	window(sf::RenderWindow(sf::VideoMode(windowSize.x, windowSize.y), title, style))
 {
-	// create window
-	window.setFramerateLimit(60);
-	window.setVerticalSyncEnabled(true);
+	limitFrameRate(true);
 
 	// set icon
-	setWindowIcon();
+	setWindowIcon("icon.png");
 
 	// create logger
 	Logger::createLogger(std::cout, Logger::DEBUG);
@@ -26,7 +24,77 @@ Game::Game(const sf::Vector2i &windowSize, const sf::Uint32 &style) :
 		exit(-1);
 	}
 
+	// load fps counter
+	initFPSDisplay(fps);
+
 	Logger::logDebug("Game started");
+}
+
+void BaseGame::beginGame()
+{
+	start();
+
+	sf::Clock clock;
+	sf::Event e;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed || (e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Escape))
+				window.close();
+			else
+				handleInput(e);
+		}
+
+		float delta(clock.restart().asSeconds());
+		tick(delta);
+
+		window.clear();
+		render();
+
+		if (showFPS)
+		{
+			fps.setString(std::to_string(static_cast<int>(1 / delta)));
+			window.draw(fps);
+		}
+
+		window.display();
+	}
+
+	end();
+}
+
+void BaseGame::limitFrameRate(bool limit)
+{
+	window.setFramerateLimit(limit ? 60 : 0);
+	window.setVerticalSyncEnabled(limit);
+}
+
+void BaseGame::setWindowIcon(const std::string &fileName)
+{
+	sf::Image icon;
+	if (!icon.loadFromFile(Utils::searchForFile(fileName)))
+	{
+		Logger::logWarning("Could not load icon");
+		return;
+	}
+
+	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
+
+void BaseGame::initFPSDisplay(sf::Text &fps)
+{
+	fps.setFont(Constants::mainFont);
+	fps.setCharacterSize(20);
+	fps.setPosition(20, 20);
+	fps.setColor(sf::Color::Red);
+}
+
+Game::Game(const sf::Vector2i &windowSize, const sf::Uint32 &style) : BaseGame(windowSize, style, "Dank C++ Memes")
+{
+	showFPS = true;
+	limitFrameRate(false);
 }
 
 Game::~Game()
@@ -36,37 +104,22 @@ Game::~Game()
 
 void Game::start()
 {
-	// create initial state
 	switchState(State::StateType::GAME);
+}
 
-	sf::Clock clock;
-	sf::Event event;
-	
-	sf::Text fps;
-	initFPSDisplay(fps);
+void Game::tick(float delta)
+{
+	current->tick(delta);
+}
 
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-				window.close();
-			else
-				current->handleInput(event);
-		}
+void Game::render()
+{
+	current->render(window);
+}
 
-		float delta(clock.restart().asSeconds());
-
-		current->tick(delta);
-
-		window.clear();
-		current->render(window);
-		
-		fps.setString(std::to_string(static_cast<int>(1 / delta)));
-		window.draw(fps);
-
-		window.display();
-	}
+void Game::handleInput(sf::Event e)
+{
+	current->handleInput(e);
 }
 
 void Game::switchState(State::StateType newStateType)
@@ -110,26 +163,6 @@ State* Game::createFromStateType(State::StateType type)
 	}
 }
 
-void Game::setWindowIcon()
-{
-	sf::Image icon;
-	if (!icon.loadFromFile(Utils::searchForFile("icon.png")))
-	{
-		Logger::logWarning("Could not load icon");
-		return;
-	}
-
-	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-}
-
-void Game::initFPSDisplay(sf::Text &fps)
-{
-	fps.setFont(Constants::mainFont);
-	fps.setCharacterSize(20);
-	fps.setPosition(20, 20);
-	fps.setColor(sf::Color::Red);
-}
-
 void Game::end()
 {
 	window.close();
@@ -140,8 +173,11 @@ int main()
 {
 	try
 	{
-		Game g(Constants::windowSize, sf::Style::Default);
-		g.start();
+		const sf::Vector2i windowSize = Constants::windowSize;
+		const auto style = sf::Style::Default;
+
+		Game *game = new Game(windowSize, style);
+		dynamic_cast<BaseGame*>(game)->beginGame();
 	}
 	catch (std::exception e)
 	{
--- CitySimulator/src/game.hpp
+++ CitySimulator/src/game.hpp
@@ -3,25 +3,56 @@
 #include <stack>
 #include "state.hpp"
 
-class Game
+class BaseGame
+{
+public:
+	BaseGame(const sf::Vector2i &windowSize, const sf::Uint32 &style, const std::string &title);
+
+	virtual ~BaseGame()
+	{
+	}
+	void beginGame();
+
+protected:
+	sf::RenderWindow window;
+	sf::Color backgroundColor;
+	
+	bool showFPS;
+
+	virtual void start() = 0;
+	virtual void tick(float delta) = 0;
+	virtual void render() = 0;
+	virtual void end() = 0;
+	virtual void handleInput(sf::Event e) = 0;
+
+	void limitFrameRate(bool limit);
+
+
+private:
+	void setWindowIcon(const std::string &fileName);
+	void initFPSDisplay(sf::Text &fps);
+	sf::Text fps;
+};
+
+class Game : public BaseGame
 {
 public:
 	Game(const sf::Vector2i &windowSize, const sf::Uint32 &style);
 	~Game();
 
-	void start();
-	void end();
+
+protected:
+	void start() override;
+	void end() override;
+	void tick(float delta) override;
+	void render() override;
+	void handleInput(sf::Event e) override;
+
+public:
 	void switchState(State::StateType newScreenType);
 
 private:
-	sf::RenderWindow window;
-
 	State *current;
 	std::stack<State*> states;
 	State* Game::createFromStateType(State::StateType type);
-
-	sf::Color backgroundColor;
-
-	void setWindowIcon();
-	void initFPSDisplay(sf::Text &fps);
 };
