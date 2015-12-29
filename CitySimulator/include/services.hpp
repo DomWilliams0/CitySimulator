facd28d3929f5932e599394d6401a5f67cd60925
Dom Williams <dwi3774@googlemail.com>
2015-12-29 17:14:47 +0000

    Implemented InputService

--- CitySimulator/include/services.hpp
+++ CitySimulator/include/services.hpp
@@ -2,11 +2,15 @@
 #define CITYSIM_SERVICES_HPP
 
 #include <unordered_map>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/bimap.hpp>
 
 enum ServiceType
 {
 	SERVICE_INPUT
 };
+
+class BaseService;
 
 class Locator
 {
@@ -16,7 +20,7 @@ public:
 	BaseService* locate(ServiceType type);
 
 private:
-	std::unordered_map<ServiceType, BaseService*> services;
+	std::unordered_map<ServiceType, BaseService*, std::hash<int>> services;
 
 };
 
@@ -27,6 +31,53 @@ class BaseService
 public:
 	virtual void onEnable();
 	virtual void onDisable();
+};
+
+enum InputKey
+{
+	KEY_UP,
+	KEY_LEFT,
+	KEY_DOWN,
+	KEY_RIGHT,
+	KEY_YIELD_CONTROL,
+
+	KEY_COUNT
+};
+
+class InputService : public BaseService
+{
+public:
+	virtual void onEnable() override;
+
+	/// <summary>
+	/// Updates the specified key with the given pressed value
+	/// </summary>
+	/// <param name="key">The key.</param>
+	/// <param name="press">if set to <c>true</c> [press].</param>
+	void update(sf::Keyboard::Key key, bool press);
+
+	/// <summary>
+	/// Determines whether the specified key is pressed.
+	/// </summary>
+	/// <param name="key">The key.</param>
+	/// <returns></returns>
+	bool isPressed(InputKey key);
+
+	/// <summary>
+	/// Determines whether the specified key has just been pressed.
+	/// </summary>
+	/// <param name="key">The key.</param>
+	/// <returns></returns>
+	bool isFirstPressed(InputKey key);
+
+	/// <summary>
+	/// Moves forward a timestep.
+	/// </summary>
+	void advance();
+
+private:
+	boost::bimap<InputKey, sf::Keyboard::Key> bindings;
+	boost::dynamic_bitset<> pressed, wasPressed;
 };
 
 #endif
--- CitySimulator/src/services.cpp
+++ CitySimulator/src/services.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Graphics.hpp>
 #include "logger.hpp"
 #include "utils.hpp"
 #include "services.hpp"
@@ -20,11 +21,13 @@ void Locator::provide(ServiceType type, BaseService *service)
 	Logger::logDebug(format("%1% service for service type %2%", verb, std::to_string(type)));
 }
 
-BaseService *Locator::locate(ServiceType type)
+BaseService* Locator::locate(ServiceType type)
 {
 	auto service = services.find(type);
-	return service == services.end() ? nullptr : service;
+	return service == services.end() ? nullptr : service->second;
 }
+
+// services
 
 void BaseService::onEnable()
 {
@@ -33,3 +36,54 @@ void BaseService::onEnable()
 void BaseService::onDisable()
 {
 }
+
+// input
+
+void InputService::onEnable()
+{
+	bindings.clear();
+	bindings.insert({KEY_UP, sf::Keyboard::Key::W});
+	bindings.insert({KEY_LEFT, sf::Keyboard::Key::A});
+	bindings.insert({KEY_DOWN, sf::Keyboard::Key::S});
+	bindings.insert({KEY_RIGHT, sf::Keyboard::Key::D});
+	bindings.insert({KEY_YIELD_CONTROL, sf::Keyboard::Key::Tab});
+	// todo load from config
+
+	// check all keys have been registered
+	if (bindings.left.size() != KEY_COUNT)
+	{
+		Logger::logError(format("Expected %1% key bindings, received %2% instead",
+		                        std::to_string(KEY_COUNT), std::to_string(bindings.left.size())));
+
+		error("Invalid number of key bindings");
+	}
+}
+
+void InputService::update(sf::Keyboard::Key key, bool press)
+{
+	auto inputKey = bindings.right.find(key);
+	if (inputKey != bindings.right.end())
+	{
+		int index = inputKey->second;
+		pressed[index] = press;
+	}
+}
+
+
+bool InputService::isPressed(InputKey key)
+{
+	return pressed[key];
+}
+
+bool InputService::isFirstPressed(InputKey key)
+{
+	return pressed[key] && !wasPressed[key];
+}
+
+void InputService::advance()
+{
+	for (size_t i = 0; i < KEY_COUNT; ++i)
+		wasPressed[i] = pressed[i];
+}
+
+
