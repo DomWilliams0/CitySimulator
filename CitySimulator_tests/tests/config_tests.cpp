e91b30fa61a4fb8f56fb2560c50d0bcd330c212d
Dom Williams <dwi3774@googlemail.com>
2015-12-29 15:11:35 +0000

    Added config tests

---
+++ CitySimulator_tests/tests/config_tests.cpp
@@ -0,0 +1,87 @@
+#include "gtest/gtest.h"
+#include "test_helpers.hpp"
+#include "config.hpp"
+#include "utils.hpp"
+
+struct ConfigTest : ::testing::Test
+{
+	virtual void SetUp() override
+	{
+		config = ConfigurationFile("data/test_reference_config.json");
+		config.load();
+	}
+
+	virtual void TearDown() override
+	{
+	}
+
+	ConfigurationFile config;
+};
+
+TEST(ConfigLoading, FileNotFound)
+{
+	ConfigurationFile config("no_existerino.json");
+	EXPECT_ERROR_MESSAGE(config.load();, "Config file not found: no_existerino.json");
+}
+
+TEST(ConfigLoading, NoInitialisation)
+{
+	ConfigurationFile config;
+	EXPECT_ERROR_MESSAGE(config.load();, "Config file not found: none given");
+}
+
+TEST_F(ConfigTest, SimpleValueGetting)
+{
+	int i;
+	config.getIntRef("acorn", i);
+	EXPECT_EQ(config.getInt("acorn"), 1);
+	EXPECT_EQ(i, 1);
+
+	std::string s;
+	config.getStringRef("butterfly", s);
+	EXPECT_EQ(config.getString("butterfly"), "muffin");
+	EXPECT_EQ(s, "muffin");
+
+	bool b;
+	config.getBoolRef("car-battery.inner", b);
+	EXPECT_EQ(config.getBool("car-battery.inner"), true);
+	EXPECT_EQ(b, true);
+
+	float f;
+	config.getFloatRef("override-me.pegasus", f);
+	EXPECT_EQ(config.getFloat("override-me.pegasus"), f);
+	EXPECT_EQ(f, 5.5f);
+
+	EXPECT_EQ(config.getString("car-battery.deepah.cheetah"), "woof");
+}
+
+TEST_F(ConfigTest, ComplexValueGetting)
+{
+	using namespace std;
+
+	vector<int> ints;
+	config.getList<int>("eggs.numbahs", ints);
+	EXPECT_EQ(ints.size(), 4);
+	EXPECT_EQ(ints, vector<int>({1, 2, 3, -60}));
+
+	vector<string> strings;
+	config.getList<string>("eggs.strangs", strings);
+	EXPECT_EQ(strings.size(), 5);
+	EXPECT_EQ(strings, vector<string>({"rudolph", "was", "a", "bad", "boy"}));
+
+	vector<map<string, string>> map;
+	config.getMapList("eggs.swimmers", map);
+	EXPECT_EQ(map.size(), 2);
+	EXPECT_EQ(map[0]["name"], "Boris");
+	EXPECT_EQ(map[1]["swimming"], "painful to watch");
+}
+
+TEST_F(ConfigTest, Overwriting)
+{
+	EXPECT_NE(config.getFloat("override-me.pegasus"), 100.f);
+
+	config.loadOnTop("data/test_config.json");
+
+	EXPECT_EQ(config.getFloat("override-me.pegasus"), 100.f);
+	EXPECT_EQ(config.getString("dog"), "cornichon");
+}
