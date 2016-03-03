c425cdbe28fef5f8d90b51742e9ac318f9351e92
Dom Williams <me@domwillia.ms>
2016-03-03 17:52:03 +0000

    Extracted locator implementation

--- CitySimulator/include/service/locator.hpp
+++ CitySimulator/include/service/locator.hpp
@@ -1,16 +1,7 @@
 #ifndef CITYSIM_LOCATOR_HPP
 #define CITYSIM_LOCATOR_HPP
 
-#include <typeinfo>
-#include "animation_service.hpp"
-#include "camera_service.hpp"
-#include "config_service.hpp"
-#include "entity_service.hpp"
-#include "event_service.hpp"
-#include "input_service.hpp"
-#include "logging_service.hpp"
-#include "render_service.hpp"
-#include "world_service.hpp"
+#include "base_service.hpp"
 
 class Locator
 {
@@ -21,38 +12,7 @@ public:
 	static std::string serviceToString(ServiceType type);
 
 	template<class T>
-	static T *locate(bool errorOnFail = true)
-	{
-		ServiceType type = SERVICE_UNKNOWN;
-
-		if (typeid(T) == typeid(AnimationService))
-			type = SERVICE_ANIMATION;
-		else if (typeid(T) == typeid(CameraService))
-			type = SERVICE_CAMERA;
-		else if (typeid(T) == typeid(ConfigService))
-			type = SERVICE_CONFIG;
-		else if (typeid(T) == typeid(EntityService))
-			type = SERVICE_ENTITY;
-		else if (typeid(T) == typeid(EventService))
-			type = SERVICE_EVENT;
-		else if (typeid(T) == typeid(InputService))
-			type = SERVICE_INPUT;
-		else if (typeid(T) == typeid(LoggingService))
-			type = SERVICE_LOGGING;
-		else if (typeid(T) == typeid(RenderService))
-			type = SERVICE_RENDER;
-		else if (typeid(T) == typeid(WorldService))
-			type = SERVICE_WORLD;
-		else
-			error("Invalid service type given. Has its type been registered?");
-
-		T *ret = dynamic_cast<T *>(getInstance().services[type]);
-		if (errorOnFail && ret == nullptr)
-			error("Could not locate service '%1%'", serviceToString(type));
-		return ret;
-
-
-	}
+	static T *locate(bool errorOnFail = true);
 
 private:
 	Locator();
@@ -67,5 +27,8 @@ private:
 
 	std::vector<BaseService *> services;
 };
+
+
+#include "locator.ipp"
 
 #endif
