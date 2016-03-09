4cb9ed6267ccf595431f8f2d591eef8bd38423d0
Dom Williams <me@domwillia.ms>
2016-03-09 10:43:35 +0000

    Moved Brains after Steerings

--- CitySimulator/include/ai.hpp
+++ CitySimulator/include/ai.hpp
@@ -8,76 +8,6 @@
 struct PhysicsComponent;
 
 /**
- * A wrapper around a movement controller that can be controlled either by AI or the player
- */
-class Brain
-{
-public:
-	virtual ~Brain()
-	{
-	}
-
-	/**
-	 * Changes the entity that this brain is controlling
-	 * @param e The new entity to control
-	 * @param stop If the entity should stop in his tracks
-	 */
-	void setEntity(EntityID e, bool stop = true);
-
-	void tick(float delta);
-
-
-protected:
-	EntityID entity;
-	PhysicsComponent *phys;
-	boost::shared_ptr<MovementController> controller;
-
-	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed) = 0;
-
-	virtual void tickBrain(float delta)
-	{
-	}
-
-	virtual void onEnable()
-	{
-	}
-
-	virtual void onDisable()
-	{
-	}
-};
-
-/**
- * A brain with behaviours
- */
-class EntityBrain : public Brain
-{
-public:
-	EntityBrain(EntityID e);
-
-protected:
-	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed);
-
-	void tickBrain(float delta) override;
-};
-
-/**
- * An empty brain that delegates all player inputs to the movement controller
- */
-class InputBrain : public Brain
-{
-public:
-	InputBrain(EntityID e);
-
-	virtual ~InputBrain();
-
-protected:
-	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed);
-};
-
-// steering behaviours
-
-/**
  * An interface for steering behaviours
  */
 class BaseSteering
@@ -151,6 +81,76 @@ public:
 	{ }
 
 	virtual void tick(b2Vec2 &steeringOut, float delta);
+};
+
+/**
+ * A wrapper around a movement controller that can be controlled either by AI or the player
+ */
+class Brain
+{
+public:
+	virtual ~Brain()
+	{
+	}
+
+	/**
+	 * Changes the entity that this brain is controlling
+	 * @param e The new entity to control
+	 * @param stop If the entity should stop in his tracks
+	 */
+	void setEntity(EntityID e, bool stop = true);
+
+	void tick(float delta);
+
+
+protected:
+	EntityID entity;
+	PhysicsComponent *phys;
+	boost::shared_ptr<MovementController> controller;
+
+	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed) = 0;
+
+	virtual void tickBrain(float delta)
+	{
+	}
+
+	virtual void onEnable()
+	{
+	}
+
+	virtual void onDisable()
+	{
+	}
+};
+
+// brains
+
+/**
+ * A brain with behaviours
+ */
+class EntityBrain : public Brain
+{
+public:
+	EntityBrain(EntityID e);
+
+protected:
+	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed);
+
+	void tickBrain(float delta) override;
+};
+
+/**
+ * An empty brain that delegates all player inputs to the movement controller
+ */
+class InputBrain : public Brain
+{
+public:
+	InputBrain(EntityID e);
+
+	virtual ~InputBrain();
+
+protected:
+	virtual void initController(float movementForce, float maxWalkSpeed, float maxSprintSpeed);
 };
 
 #endif
